5ea6595949bf8ba199698ac972e9746af7ce960e
Felipe Pardo <aanfepar@gmail.com>
2023-11-23 08:55:46 -0500

    feat(pagination): set first and last buttons

--- src/app/components/Pagination/Pagination.tsx
+++ src/app/components/Pagination/Pagination.tsx
@@ -1,5 +1,4 @@
 import { FETCH_ITEMS_LIMIT } from "@/app/lib/constants/numbers";
-import { useCallback, useState } from "react";
 
 interface PaginationProps {
   currentPage: number,
@@ -10,50 +9,17 @@ interface PaginationProps {
 const MAX_DESKTOP_PAGES = 6;
 
 export default function Pagination({ currentPage, totalPages, onPageClick }: PaginationProps) {
-  const [showFirstItems, setShowFirstItems] = useState(true)
   const totalButtonPages = totalPages < MAX_DESKTOP_PAGES ? totalPages : Math.ceil(totalPages / FETCH_ITEMS_LIMIT);
-
-  const buttonPages = useCallback(() => {
-    let buttonsArray;
-    if (showFirstItems) {
-      const firstPagesNumbers = Array.from({ length: MAX_DESKTOP_PAGES }, (_, index) => index + 1);
-      const lastPagesNumbers = [-1, totalButtonPages]
-      buttonsArray = [...firstPagesNumbers, ...lastPagesNumbers];
-    } else {
-      const firstPagesNumbers = [1, -1]
-      const lastPagesNumbers = Array.from({ length: MAX_DESKTOP_PAGES }, (_, index) => totalButtonPages - index).reverse();
-      buttonsArray = [...firstPagesNumbers, ...lastPagesNumbers];
-    }
-
-    return buttonsArray.map(page => {
-      let buttonText: number | string = page
-      let action = () => onPageClick(page)
-      if (page < 0) {
-        buttonText = "..."
-        action = () => {
-          onPageClick(totalPages - MAX_DESKTOP_PAGES - 1)
-          setShowFirstItems(!showFirstItems)
-        }
-      }
-      return (
-        <tr key={page}>
-          <td>
-            <button onClick={action}>
-              {buttonText}
-            </button>
-          </td>
-        </tr>
-      )
-    })
-  }, [totalPages, onPageClick, showFirstItems, totalButtonPages])
 
   if (!totalPages) return null
   return (
-    <div>
-      <ul>
-        <li><button onClick={() => onPageClick(currentPage - 1)}>prev</button></li>
-        {buttonPages()}
-        <li><button onClick={() => onPageClick(currentPage + 1)}>next</button></li>
+    <div className="flex">
+      <ul className="flex">
+        <li><button onClick={() => onPageClick(1)}>first</button></li>
+        <li><button onClick={() => currentPage > 1 ? onPageClick(currentPage - 1) : null}>prev</button></li>
+        <li><span>{currentPage}</span></li>
+        <li><button onClick={() => currentPage < totalButtonPages ? onPageClick(currentPage + 1) : null}>next</button></li>
+        <li><button onClick={() => onPageClick(totalButtonPages)}>last</button></li>
       </ul>
     </div>
 

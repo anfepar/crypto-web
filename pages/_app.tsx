a0d0a19f05a95427db20caf261c6d0fa05650127
Felipe Pardo <aanfepar@gmail.com>
2023-11-30 22:25:31 -0500

    fix: add import of fontawesome css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import type { AppProps } from 'next/app'
 import { wrapper } from '../lib/store'
 import { Provider } from 'react-redux'
 import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
 import Layout from '@/components/Layout/Layout'
 import '@/style/globals.css'
 config.autoAddCss = false
